6aa5b7be91a76bf6bf8d38f42dba70acca0b731e
RBBK7 <rbarahona2058@gmail.com>
2024-11-24 11:59:52 -0600

    Semana 7
    Inclusión de firebase, modificar y eliminar categoria

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,5 @@
 package com.tienda.service.impl;
+
 import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
@@ -6,11 +7,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
+
     @Autowired
     private CategoriaDao categoriaDao;
+
     @Override
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activos) {
@@ -19,5 +22,23 @@ public class CategoriaServiceImpl implements CategoriaService {
            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+    
+    @Override
+    @Transactional(readOnly=true)
+    public Categoria getCategoria(Categoria categoria){
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+    
+    @Override
+    @Transactional
+    public void save(Categoria categoria){
+        categoriaDao.save(categoria);
+    }
+    
+    @Override
+    @Transactional
+    public void delete(Categoria categoria){
+        categoriaDao.delete(categoria);
     }
 }
